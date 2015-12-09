182ae244310db6ed3d95c4170490c44c94cdc36f
ekuiwag <kui.k.wang@ericsson.com>
2015-12-09 15:19:52 +0800

    add Averrage.java in src/chapter7

---
+++ javanote/src/chapter7/cc/openhome/Average.java
@@ -0,0 +1,28 @@
+package cc.openhome;
+
+import java.util.*;
+
+public class Average {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int number;
+            double sum = 0;
+            int count = 0;
+            while(true) {
+                number = scanner.nextInt();
+                if(number == 0) {
+                    break;
+                }
+                sum += number;
+                count++;
+            }
+            System.out.printf("Average value = %.2f%n", sum / count);
+        }   catch (InputMismatchException ex) {
+            System.out.println("must input a inter");
+        }
+    }
+
+}
